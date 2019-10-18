24b8a1562f0863c3a0ad634fd844f78d2bc6fa93
Alice Freda <alicefreda@gmail.com>
2019-10-18 00:44:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,8 +3,13 @@
 #   "Sarah" => 6,
 #   "Amanda" => 27
 # }
-
 def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
